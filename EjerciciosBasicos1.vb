9fbf5f0b5d23e537b2e630b1828c0420dc75622b
deivid <101598816+deividbautista@users.noreply.github.com>
2023-03-17 01:27:40 -0500

    cuarto cambio

--- EjerciciosBasicos1.vb
+++ EjerciciosBasicos1.vb
@@ -1,4 +1,5 @@
-﻿Module Module1
+﻿Imports System.Math
+Module Module1
     Sub Main()
         '---------------------------------------------
         'Numero par o impar
@@ -84,6 +85,88 @@
             Console.WriteLine("El número" & númeroprimo & " no es primo :U")
 
         End If
+
+        Console.WriteLine("------------------------------------------------------------------ ")
+        '---------------------------------------------
+        'Hallar los grados de un triangulo 
+        '---------------------------------------------
+
+        Console.WriteLine("Hallar grados de un triangulo :} ")
+
+
+        Dim A As Double
+        Dim B As Double
+        Dim C As Double
+        Dim AB(2) As Double
+        Dim BC(2) As Double
+        Dim AC(2) As Double
+        Dim VectoresAB As Double
+        Dim vectoresBC As Double
+        Dim vectoresAC As Double
+        Dim escalarA As Integer
+        Dim escalarB As Integer
+        Dim paso1 As Double
+        Dim paso2 As Double
+        Dim paso3 As Double
+        Dim cosenor As Double
+        Dim división As Double
+        Dim Ax As Integer
+        Dim Ay As Integer
+        Dim Bx As Integer
+        Dim By As Integer
+        Dim Cx As Integer
+        Dim Cy As Integer
+
+
+        Console.WriteLine("Digite la cordenada X del vertice A: ")
+        Ax = Console.ReadLine()
+        Console.WriteLine("Digite la cordenada y del vertice A: ")
+        Ay = Console.ReadLine()
+        Console.WriteLine("Digite la cordenada x del vertice B: ")
+        Bx = Console.ReadLine()
+        Console.WriteLine("Digite la cordenada y del vertice B: ")
+        By = Console.ReadLine()
+        Console.WriteLine("Digite la cordenada x del vertice C: ")
+        Cx = Console.ReadLine()
+        Console.WriteLine("Digite la cordenada y del vertice C: ")
+        Cy = Console.ReadLine()
+
+        AB(0) = Bx - Ax
+        AB(1) = By - Ay
+        AC(0) = Cx - Ax
+        AC(1) = Cy - Ay
+        BC(0) = Cx - Bx
+        BC(1) = Cy - By
+        VectoresAB = AB(0) ^ 2 + AB(1) ^ 2
+        vectoresAC = AC(0) ^ 2 + AC(1) ^ 2
+        vectoresBC = BC(0) ^ 2 + BC(1) ^ 2
+
+        escalarA = AB(0) * AC(0) + AB(1) * AC(1)
+        escalarB = AB(0) * BC(0) + AB(1) * BC(1)
+
+        'Atan(-x / Sqrt(-x * x + 1)) + (2 * Atan(1))
+
+        cosenor = Sqrt(VectoresAB) * Sqrt(vectoresAC)
+        división = escalarA / cosenor
+        paso1 = -división * división + 1
+        paso2 = Atan(-división / Sqrt(paso1))
+        paso3 = paso2 * 180 / Math.PI
+        A = paso3 + (2 * Atan(1) * 180 / Math.PI)
+
+
+        cosenor = Sqrt(VectoresAB) * Sqrt(vectoresBC)
+        división = escalarB / cosenor
+        división = Math.Abs(división)
+        paso1 = -división * división + 1
+        paso2 = Atan(-división / Sqrt(paso1))
+        paso3 = paso2 * 180 / Math.PI
+        B = paso3 + (2 * Atan(1) * 180 / Math.PI)
+
+        C = 180 - A - B
+        Console.WriteLine("El angulo del vertice A es: " & A)
+        Console.WriteLine("El angulo del vertice B es: " & B)
+        Console.WriteLine("El angulo del vertice C es: " & C)
+
 
         'Utilizamos la siguiente linea para evitar que el ejecutable se suependa una vez acabado el proceso
         Console.ReadKey(True)
